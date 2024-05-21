cd294aeaf5eada3fc752967ba84b5b167ac839c5
DireElf <3672@mail.ru>
2024-05-21 17:18:10 +0300

    task3 done

---
+++ task3/src/Task3.java
@@ -0,0 +1,145 @@
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+public class Task3 {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // игнорируются поля со значением null
+            Tests tests = mapper
+                    .readValue(Paths.get(args[0]).toFile(), Tests.class); // тестовый файл resources/tests.json
+            Values values = mapper
+                    .readValue(Paths.get(args[1]).toFile(), Values.class); // тестовый файл resources/values.json
+
+            fillTestsWithResults(tests.getTests(), values.getValues());
+
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(Paths.get(args[2]).toFile(), tests); // тестовый файл resources/report.json
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Метод заполняет поля value каждого теста при наличии значения
+     * @param tests - набор тестов
+     * @param values - набор значений
+     */
+    private static void fillTestsWithResults(List<Test> tests, List<Value> values) {
+        for (Test test : tests) {
+            Optional<String> testResult = findTestResultById(values, test.getId());
+            testResult.ifPresent(test::setValue);
+            if (test.getValues() != null) {
+                fillTestsWithResults(test.getValues(), values);
+            }
+        }
+    }
+
+    /**
+     * Метод предназначен для поиска результата теста в наборе значений по идентификатору теста
+     * @param list - список результатов тестов
+     * @param id - идентификатор теста
+     * @return Optional со значением результата
+     */
+    private static Optional<String> findTestResultById(List<Value> list, int id) {
+        for (Value value : list) {
+            if (value.id == id) {
+                return Optional.of(value.value);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /*
+    POJO classes
+     */
+    private static class Test {
+        private int id;
+        private String title;
+        private String value;
+        private List<Test> values;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public List<Test> getValues() {
+            return values;
+        }
+
+        public void setValues(List<Test> values) {
+            this.values = values;
+        }
+    }
+
+    private static class Value {
+        private int id;
+        private String value;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    private static class Tests {
+        private List<Test> tests;
+
+        public List<Test> getTests() {
+            return tests;
+        }
+
+        public void setTests(List<Test> tests) {
+            this.tests = tests;
+        }
+    }
+
+    private static class Values {
+        private List<Value> values;
+
+        public List<Value> getValues() {
+            return values;
+        }
+
+        public void setValues(List<Value> values) {
+            this.values = values;
+        }
+    }
+}
